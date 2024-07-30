a5ec17a1ad7039e6ee8a6814d8a8715bd2316ec1
Reem Khalifa <reemkhalifa110@gmail.com>
2024-07-30 13:03:27 -0400

    Initial Commit

---
+++ yourBigSister/yourBigSisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  yourBigSisterApp.swift
+//  yourBigSister
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct yourBigSisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
