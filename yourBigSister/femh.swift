c98ab501f66ac77463701fbf07981bdc1be80d94
Reem Khalifa <reemkhalifa110@gmail.com>
2024-07-30 15:17:01 -0400

    buttons href

---
+++ yourBigSister/femh.swift
@@ -0,0 +1,18 @@
+//
+//  femh.swift
+//  yourBigSister
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct femh: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    femh()
+}
---
+++ yourBigSister/repo.swift
@@ -0,0 +1,18 @@
+//
+//  repo.swift
+//  yourBigSister
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct repo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    repo()
+}
